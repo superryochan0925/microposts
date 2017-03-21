2c9791accb0b3a2127fbf451c414dd81a7bdf9e3
ryo takeda <r.takeda0925@gmail.com>
2017-03-21 12:10:43 +0000

    add posting microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ActiveRecord::Base
   validates :location, allow_blank: true, length: { minimum: 2, maximum: 100 }
   validates :gender, presence: true, on: :update
   has_secure_password
+  has_many :microposts
 end
